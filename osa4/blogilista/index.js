dc87782c4216813bd3dd962ffb40d60bae59812f
ville suokas <vilsuo1@gmail.com>
2020-02-15 21:46:34 +0200

    tehtävä 4.1 toimiva

---
+++ osa4/blogilista/index.js
@@ -0,0 +1,51 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const password = process.argv[2]
+
+const Blog = mongoose.model('Blog', blogSchema)
+const mongoUrl = `mongodb+srv://fullstack:${password}@cluster0-20mf4.mongodb.net/blogs?retryWrites=true&w=majority`
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDb:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
