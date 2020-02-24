d516fb5f2c9cc44bf50e257374450d9e83d8b08a
ville suokas <vilsuo1@gmail.com>
2020-02-24 02:59:08 +0200

    osa5 toimiva 5.4 ast

---
+++ osa5/bloglist-frontend/src/App.js
@@ -0,0 +1,177 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [message, setMessage] = useState('')
+
+  const [user, setUser] = useState(null)
+
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      window.localStorage.setItem(
+        'loggedBlogappUserToken', JSON.stringify(user.token)
+      )
+
+      blogService.setToken(user.token)
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      
+    } catch (error) {
+      showMessage(error.response.data.error)
+    }
+  }
+
+  const handleLogout = (event) => {
+    event.preventDefault()
+    window.localStorage.removeItem('loggedBlogappUser')
+    window.localStorage.removeItem('loggedBlogappUserToken')
+    setUser(null)
+    blogService.setToken(null)
+  }
+
+  const handleBlogPost = async (event) => {
+    event.preventDefault()
+
+    const blogToPost = {
+      title: title,
+      author: author,
+      url: url
+    }
+
+    try {
+      const result = await blogService.create(blogToPost)
+      setBlogs(blogs.concat(result))
+      showMessage(`a new blog ${result.title} by ${result.author} added`)
+
+    } catch (error) {
+      showMessage(error.response.data.error)
+    }
+
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+  }
+
+  const showMessage = message => {
+    setMessage(message)
+
+    setTimeout(() => {
+      setMessage(null)
+    }, 5000)
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        {message}
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+              <input 
+                type="text"
+                value={username}
+                name="Username"
+                onChange={({ target }) => setUsername(target.value)}
+              />
+          </div>
+          <div>
+            password
+              <input
+                type="text"
+                value={password}
+                name="Password"
+                onChange={({ target }) => setPassword(target.value)}
+              />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      <p>{message}</p>
+      <div>
+        {user.username} logged in
+        <button type="submit" onClick={handleLogout}>logout</button>
+      </div>
+      <h2>create new</h2>
+      <div>
+        <form onSubmit={handleBlogPost}>
+          <div>
+            title
+              <input
+                type="text"
+                value={title}
+                name="Title"
+                onChange={({ target }) => setTitle(target.value)}
+              />
+          </div>
+          <div>
+            author
+              <input
+                type="text"
+                value={author}
+                name="Author"
+                onChange={({ target }) => setAuthor(target.value)}
+              />
+          </div>
+          <div>
+            url
+              <input
+                type="text"
+                value={url}
+                name="Url"
+                onChange={({ target }) => setUrl(target.value)}
+              />
+          </div>
+          <button type="submit">create</button>
+        </form>
+      </div>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
